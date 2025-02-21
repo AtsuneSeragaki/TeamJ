aa8c789dae31ced195f407811c86e1cba9559491
KaWeco258 <138412692+KaWeco258@users.noreply.github.com>
2025-02-21 15:23:11 +0900

    Ranking 追加

--- TeamJ/TeamJ/Scene/Ranking/RankingScene.cpp
+++ TeamJ/TeamJ/Scene/Ranking/RankingScene.cpp
@@ -1,4 +1,5 @@
 #include "RankingScene.h"
+#include <DxLib.h>
 
 // コンストラクタ
 RankingScene::RankingScene()
@@ -13,6 +14,8 @@ RankingScene::~RankingScene()
 // 初期化処理
 void RankingScene::Initialize()
 {
+	//仮の名前
+	char handlename[5] = { 'maly','john','ken','roy','luke' };
 }
 
 // 更新処理
@@ -24,6 +27,15 @@ eSceneType RankingScene::Update()
 // 描画処理
 void RankingScene::Draw() const
 {
+	SetFontSize(20);
+	DrawString(10, 10, "RANKING", 0xffffff);
+
+	//SetFontSize(50);
+	for (int i = 1; i < 6; ++i)
+	{
+		//DrawFormatString(50, 70 + i * 60, GetColor(255, 255, 255), "No.%d %4s %d", i, handlename[i - 1], 600 - (i * 100));
+		DrawFormatString(50, 70 + i * 60, GetColor(255, 255, 255), "No.%d  %d", i, 600 - (i * i + 10 * i));
+	}
 }
 
 // 終了時処理
--- TeamJ/TeamJ/Scene/Ranking/RankingScene.h
+++ TeamJ/TeamJ/Scene/Ranking/RankingScene.h
@@ -4,6 +4,11 @@
 
 class RankingScene : public SceneBase
 {
+private:
+	int score[6];       //スコアデータ
+	int rank[6];        //ランクデータ
+	char name[6][15];   //名前データ
+	char handlename[5];   //名前データ(仮)
 
 public:
 	// コンストラクタ
@@ -20,6 +25,10 @@ public:
 	virtual void Draw() const override;
 	// 終了時処理
 	virtual void Finalize() override;
+
+private:
+	//データ入れ替え処理
+	void SortData();
 
 public:
 	// 現在のシーン情報を返す
